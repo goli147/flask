d218232aac4ccc6046a330571eaa610b87f00379
Goli <usama@Usamas-MacBook-Air.local>
2022-07-07 04:56:52 +0500

    ebs

--- application.py
+++ application.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+application = Flask(__name__)
 
-@app.route('/')
+@application.route('/')
 def hello_world():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0')
+    application.run(debug=True,host='0.0.0.0')
     
